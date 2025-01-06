7c9c9d1c8b7ecb4c469290d9d8864b49b6ffaf34
DevboiDesigns <75514458+DevboiDesigns@users.noreply.github.com>
2025-01-07 04:41:45 +0700

    config for using vue views

---
+++ src/TestView.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>Test View</h1>
+    <p>This is my test view</p>
+  </div>
+</template>
--- src/one-element.ts
+++ src/one-element.ts
@@ -1,12 +1,12 @@
 import { LitElement, html } from "lit"
 import { customElement, property } from "lit/decorators.js"
-
+import "./test-view-wrapper"
 @customElement("one-element")
 class OneElement extends LitElement {
   @property({ type: String }) oneName = "Default Company Name"
 
   render() {
-    return html`<pre>${this.oneName}</pre>`
+    return html`<test-view-wrapper></test-view-wrapper>`
   }
 }
 
